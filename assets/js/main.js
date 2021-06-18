f185ef8a1dae31846759bf22d72745a8422d218f
TonTon69 <hoanghuynh0609@gmail.com>
2021-06-18 15:33:35 +0700

    slider store

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+    var swiper = new Swiper(".swiper-container", {
+        slidesPerView: 5,
+        spaceBetween: 0,
+        autoplay: {
+            delay: 4000,
+        },
+        navigation: {
+            nextEl: ".right-button",
+            prevEl: ".left-button",
+        },
+    });
     $(window).scroll(function () {
         if (this.scrollY > 20) {
             $(".header .navbar").addClass("sticky");
