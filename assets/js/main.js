a6d6e80447500cd930b4501f04ec20c0bac1bba0
TonTon69 <hoanghuynh0609@gmail.com>
2021-06-19 13:13:41 +0700

    login & register

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    // scroll navbar
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $(".header .navbar").addClass("sticky");
+        } else {
+            $(".header .navbar").removeClass("sticky");
+        }
+    });
+    //slider
     var swiper = new Swiper(".swiper-container", {
         slidesPerView: 5,
         spaceBetween: 0,
@@ -10,11 +19,12 @@ $(document).ready(function () {
             prevEl: ".left-button",
         },
     });
-    $(window).scroll(function () {
-        if (this.scrollY > 20) {
-            $(".header .navbar").addClass("sticky");
-        } else {
-            $(".header .navbar").removeClass("sticky");
-        }
-    });
+    //active link
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get("area_id");
+    if (myParam === null) {
+        document.getElementById("all").classList.add("active");
+    } else {
+        document.getElementById(myParam).classList.add("active");
+    }
 });
