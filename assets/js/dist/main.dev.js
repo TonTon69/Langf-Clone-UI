9c678f5aa642bb917790c9baf2f9aba2b1c6e864
TonTon69 <hoanghuynh0609@gmail.com>
2021-06-27 18:29:19 +0700

    service

---
+++ assets/js/dist/main.dev.js
@@ -0,0 +1,43 @@
+"use strict";
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // scroll navbar
+    if (this.scrollY > 20) {
+      $(".header .navbar").addClass("sticky");
+    } else {
+      $(".header .navbar").removeClass("sticky");
+    } // scroll show button back to top
+
+
+    if (this.scrollY > 500) {
+      $(".back-to-top").addClass("show");
+    } else {
+      $(".back-to-top").removeClass("show");
+    }
+  }); // back to top
+
+  $(".back-to-top").click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+  }); // active link
+
+  $(".areas .list-group .list-group-item").click(function (e) {
+    e.preventDefault();
+    $(this).addClass("active").siblings().removeClass("active");
+  }); // slider
+
+  var swiper = new Swiper(".swiper-container", {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 0,
+    autoplay: {
+      delay: 4000
+    },
+    navigation: {
+      nextEl: ".right-button",
+      prevEl: ".left-button"
+    }
+  });
+});
